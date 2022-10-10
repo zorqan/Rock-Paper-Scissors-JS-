c254a74f9b80baeb5e697a1c7ac97358af73f5f5
zorqan <99579497+zorqan@users.noreply.github.com>
2022-10-10 22:36:04 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 // This is our little game Rock Paper Scissors!
+// We shall begin with declaring some
+// variables!!!
+
+let  playerScore = 0;
+
+let computerScore = 0;
+ 
 
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+
+
 
 // This function randomly picks "Rock, Paper, or Scissors" 
 //and also returns the string in lower case
@@ -15,33 +22,86 @@ function getComputerChoice() {
   return lowerCaseChoice;
 } 
 
+// This function asks player to choose one of 3 "Rock, Paper or Scissors"
+// and returns lower case strings
+function playerSelection() {
+  let choose = prompt("Please chose one of: Paper, Rock or Scissors")
+  let lowerChose = choose.toLocaleLowerCase();
+  return lowerChose;
+}
+
+
+
 // Now we have to write a function that plays one round and declares a winner of that
 // round by saying : "paper beats rock" or whatever!
 
-function playRound(playerSelection,computerSelection) {
-  if(playerSelection === computerSelection){
+function playRound(playerSelect,computerSelection) {
+  console.log("Player chose : " + playerSelect);
+
+  console.log("Computer chose : " + computerSelection);
+
+  if(playerSelect === computerSelection){
     return "Tie! neither side gets points"
-  }else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return `You win!  ${playerSelection}  beats ${computerSelection}` 
-  }else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return `You win!  ${playerSelection}  beats ${computerSelection}` 
-  }else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    return `You win!  ${playerSelection}  beats ${computerSelection}` 
-  }else if (playerSelection == 'rock' && computerSelection == 'paper') {
-    return `You loose!  ${computerSelection}  beats ${playerSelection}` 
-  }else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-    return `You win!  ${computerSelection}  beats ${playerSelection}` 
-  }else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-    return `You win!  ${computerSelection}  beats ${playerSelection}` 
+  }else if (playerSelect == 'rock' && computerSelection == 'scissors') {
+    playerScore++
+    return `You win!  ${playerSelect}  beats ${computerSelection}` 
+     
+  }else if (playerSelect == 'paper' && computerSelection == 'rock') {
+    playerScore++
+    return `You win!  ${playerSelect}  beats ${computerSelection}` 
+     
+  }else if (playerSelect == 'scissors' && computerSelection == 'paper') {
+    playerScore++
+    return `You win!  ${playerSelect}  beats ${computerSelection}` 
+     
+  }else if (playerSelect == 'rock' && computerSelection == 'paper') {
+    computerScore++
+    return `You loose!  ${computerSelection}  beats ${playerSelect}` 
+     
+  }else if (playerSelect == 'paper' && computerSelection == 'scissors') {
+    computerScore++
+    return `You loose!  ${computerSelection}  beats ${playerSelect}` 
+     
+  }else if (playerSelect == 'scissors' && computerSelection == 'rock') {
+    computerScore++
+    return `You loose!  ${computerSelection}  beats ${playerSelect}` 
+  }else if (playerSelect !== 'scissors' || playerSelect !== 'paper' || playerSelect !== 'rock') {
+    
+    return "You have to chose: Paper Rock or Scissors. Please start again!"
   }
+  
+
+  
 }
 
 
 
 
-console.log("Player chose : " + playerSelection);
-console.log("Computer chose : " + computerSelection);
-console.log(playRound(playerSelection,computerSelection));
 
- 
+
+// Now we have to create a function in
+// which game will play 5 rounds and 
+// declares the final winner of the game
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    
+    const playerSelect = playerSelection();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelect,computerSelection);
+    console.log('player score is : ' + playerScore);
+    console.log('computer score is : ' + computerScore);
+    
+    
+  }
+  if (playerScore > computerScore) {
+    return "You won"
+  }else if (computerScore > playerScore) {
+    return "Computer won"
+  }else{
+    return "It is tie, nobody won"
+  }
+}
+console.log(game());
+
 
